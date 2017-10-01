d88a453a3de3dd4eb7a2bde07dd6d5a5253ada46
yurisasc <yuris_aryansyah@yahoo.com>
2017-10-01 21:03:50 +0700

    fixed syntax

--- echobot/views.py
+++ echobot/views.py
@@ -6,7 +6,7 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
 from linebot.models import MessageEvent, TextSendMessage, TextMessage
 
-from methods import semangat
+from .methods import semangat
 
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(settings.LINE_CHANNEL_SECRET)
