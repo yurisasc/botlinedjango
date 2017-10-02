18ce992a3f06c2ab006be1558293a4436563e228
yurisasc <yuris_aryansyah@yahoo.com>
2017-10-02 13:33:45 +0700

    moved chromedriver dir

--- echobot/tests.py
+++ echobot/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from selenium import webdriver
+import unittest
+import time
 
-# Create your tests here.
+class NewVisitortest(unittest.TestCase):
+    def setUp(self):
+            self.browser = webdriver.Chrome('./chromedriver.exe')
+    
+    def tearDown(self):
+            self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+            self.browser.get('http://www.google.com/xhtml')
+            time.sleep(5)
+
+            search_box = self.browser.find_element_by_name('q')
+            search_box.send_keys('Gunung Agung')
+            search_box.submit()
+            time.sleep(5)
+            self.assertIn('Bali',self.browser.page_source)
+
+if __name__ == '__main__':#
+	unittest.main(warnings='ignore')#
