5b6c325515ed1e05ace2a4f3470949a4410653b3
yurisasc <yuris_aryansyah@yahoo.com>
2017-10-03 14:11:14 +0700

    debug all vars for get_name

--- echobot/methods.py
+++ echobot/methods.py
@@ -115,9 +115,12 @@ def button(event, line_bot_api):
 def get_name(event, line_bot_api):
     print(event.source.group_id)
     groupId = event.source.group_id
+    print(groupId)
     userId = event.source.sender_id
+    print(userId)
     profile = line_bot_api.get_group_member_profile(groupId, userId)
     name = profile.display_name
+    print(name)
     print("group_id: "+groupId+", userId: "+userID+", name: "+name)
 ##    if(isinstance(event.source, SourceUser)):
 ##        userId = event.source.sender_id
